a347004455f20a1b8bcbe8898d16392a0d4502f8
Cyrus Goh <hello@lovincyrus.com>
2021-03-09 12:57:43 -0800

    add /cosmos/tag/tutorial

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const
   cache = require("./cache"),
   HTMLParser = require('node-html-parser'),
   _ = require("lodash"),
-  app = express()
+  app = express(),
+  port = process.env.PORT || 8888;
 
 app.use(bodyParser.json());
 
@@ -38,6 +39,28 @@ app.get('/medium/?', cache.serve(30), function (req, res) {
   })
 });
 
-const listener = app.listen(process.env.PORT, function () {
+app.get('/cosmos/tag/tutorial', cache.serve(30), function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  axios.all([
+    axios.get("https://blog.cosmos.network/feed")
+  ]).then(responses => {
+    const all = responses.map(e => {
+      return JSON.parse(convert.xml2json(e.data, { compact: true })).rss.channel.item.map(item => {
+        return {
+          title: item.title._cdata,
+          link: item.link._text,
+          date: item.pubDate._text,
+          image: HTMLParser.parse(item["content:encoded"]._cdata).querySelector("img").attributes['src'].toString(),
+          timestamp: new Date(item.pubDate._text).getTime(),
+          category: item.category._cdata || item.category.map(i => i._cdata)
+        }
+      })
+    }).flat(1)
+    res.status(200).send(all)
+  })
+  .catch(error => console.log(error))
+});
+
+const listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
